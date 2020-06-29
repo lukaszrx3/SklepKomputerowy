22695d0ff6bc991075066793cc307240e268a977
Lukasz lukasz <lukaszxr3@outlook.com>
2020-06-29 09:32:42 +0200

    Dodano testowa liste produktow

--- SklepKomputerowy/Pages/ProductsPage.xaml.cs
+++ SklepKomputerowy/Pages/ProductsPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SklepKomputerowy.ViewModels;
 
 namespace SklepKomputerowy.Pages
 {
@@ -21,11 +22,102 @@ namespace SklepKomputerowy.Pages
     public partial class ProductsPage : Page
     {
         private SklepKomputerowyDbEntities db;
+        public List<Product> Products;
 
         public ProductsPage()
         {
             InitializeComponent();
             db = new SklepKomputerowyDbEntities();
+
+            Products = new List<Product>
+            {
+                new Product
+                {
+                    Code = "ROC1",
+                    Name = "Razer Ornata Chroma",
+                    Description = "Piękne podświetlenie RGB, wysoki skok klawiszy idealna gamingowa klawiatura, kabel USB solidny w oplocie. Polecam po ponad roku testów.",
+                    Price = 299,
+                    WarrantyMonths = 24,
+                    ImgUrl = "https://f01.esfr.pl/foto/6/14256563153/ca6518eb7506475bd2b34888e7358a8a/razer-ornata-chroma,14256563153_7.jpg",
+                    ExtraParametersDictionary = new Dictionary<string, string>
+                    {
+                        {"Podswietlana", "tak" },
+                        {"Zasilanie", "USB" },
+                        {"Rodzaj", "Przewodowa" }
+
+                    }
+
+                },
+                new Product
+                {
+                    Code = "ROC2",
+                    Name = "22222222Razer Ornata Chroma2",
+                    Description = "2222Piękne podświetlenie RGB, wysoki skok klawiszy idealna gamingowa klawiatura, kabel USB solidny w oplocie. Polecam po ponad roku testów.",
+                    Price = 299,
+                    WarrantyMonths = 24,
+                    ImgUrl = "https://f01.esfr.pl/foto/6/14256563153/7023bf5940d84b19807f50e029adabbb/razer-ornata-chroma,14256563153_3.jpg",
+                    ExtraParametersDictionary = new Dictionary<string, string>
+                    {
+                        {"Podswietlana", "nie" },
+                        {"Zasilanie", "bateria" },
+                        {"Rodzaj", "na baterie" }
+
+                    }
+
+                },
+                new Product
+                {
+                    Code = "ROC3",
+                    Name = "Razer Ornata Chroma",
+                    Description = "Piękne podświetlenie RGB, wysoki skok klawiszy idealna gamingowa klawiatura, kabel USB solidny w oplocie. Polecam po ponad roku testów.",
+                    Price = 299,
+                    WarrantyMonths = 24,
+                    ImgUrl = "https://f01.esfr.pl/foto/6/14256563153/ca6518eb7506475bd2b34888e7358a8a/razer-ornata-chroma,14256563153_7.jpg",
+                    ExtraParametersDictionary = new Dictionary<string, string>
+                    {
+                        {"Podswietlana", "tak" },
+                        {"Zasilanie", "USB" },
+                        {"Rodzaj", "Przewodowa" }
+
+                    }
+
+                },
+                new Product
+                {
+                    Code = "ROC4",
+                    Name = "Razer Ornata Chroma",
+                    Description = "Piękne podświetlenie RGB, wysoki skok klawiszy idealna gamingowa klawiatura, kabel USB solidny w oplocie. Polecam po ponad roku testów.",
+                    Price = 299,
+                    WarrantyMonths = 24,
+                    ImgUrl = "https://f01.esfr.pl/foto/6/14256563153/ca6518eb7506475bd2b34888e7358a8a/razer-ornata-chroma,14256563153_7.jpg",
+                    ExtraParametersDictionary = new Dictionary<string, string>
+                    {
+                        {"Podswietlana", "tak" },
+                        {"Zasilanie", "USB" },
+                        {"Rodzaj", "Przewodowa" }
+
+                    }
+
+                },
+                new Product
+                {
+                    Code = "Display1",
+                    Name = "Monitor iiyama",
+                    Description = "Piękne podświetlenie RGB, wysoki skok klawiszy idealna gamingowa klawiatura, kabel USB solidny w oplocie. Polecam po ponad roku testów.",
+                    Price = 799,
+                    WarrantyMonths = 12,
+                    ImgUrl = "https://image.ceneostatic.pl/data/products/47995171/i-iiyama-24-prolite-xu2492hsu-b1.jpg",
+                    ExtraParametersDictionary = new Dictionary<string, string>
+                    {
+                        {"Podswietlana", "tak" },
+                        {"Zasilanie", "USB" },
+                        {"Rodzaj", "Przewodowa" }
+
+                    }
+
+                },
+            };
+            ListViewProducts.ItemsSource = Products;
         }
 
 
--- SklepKomputerowy/ViewModels/Product.cs
+++ SklepKomputerowy/ViewModels/Product.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace SklepKomputerowy.ViewModels
 {
-    class Product
+    public class Product
     {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public int WarrantyMonths { get; set; }
+        public decimal Price { get; set; }
+        public Dictionary<string, string> ExtraParametersDictionary { get; set; }
+        public string ImgUrl { get; set; }
     }
 }
 
